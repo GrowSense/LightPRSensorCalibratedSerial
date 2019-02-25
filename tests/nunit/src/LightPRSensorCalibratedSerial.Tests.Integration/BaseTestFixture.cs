29b617d6c708432c61fe82fffbef8c6ecd65e51e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-25 02:50:30 +0000

    Fixes and updated arduino serial controller client

--- tests/nunit/src/LightPRSensorCalibratedSerial.Tests.Integration/BaseTestFixture.cs
+++ tests/nunit/src/LightPRSensorCalibratedSerial.Tests.Integration/BaseTestFixture.cs
@@ -6,93 +6,90 @@ using System.IO;
 
 namespace LightPRSensorCalibratedSerial.Tests.Integration
 {
-	public class BaseTestFixture
-	{
-		public BaseTestFixture()
-		{
-		}
+    public class BaseTestFixture
+    {
+        public BaseTestFixture ()
+        {
+        }
 
-		[SetUp]
-		public void Initialize()
-		{
-			Console.WriteLine("");
-			Console.WriteLine("====================");
-			Console.WriteLine("Preparing test");
-		}
+        [SetUp]
+        public void Initialize ()
+        {
+            Console.WriteLine ("");
+            Console.WriteLine ("====================");
+            Console.WriteLine ("Preparing test");
+        }
 
-		[TearDown]
-		public virtual void Finish()
-		{
-			HandleFailureFile();
+        [TearDown]
+        public virtual void Finish ()
+        {
+            HandleFailureFile ();
 
-			Console.WriteLine("Finished test");
-			Console.WriteLine("====================");
-			Console.WriteLine("");
-		}
+            Console.WriteLine ("Finished test");
+            Console.WriteLine ("====================");
+            Console.WriteLine ("");
+        }
 
-		public void HandleFailureFile()
-		{
-			var failuresDir = Path.GetFullPath("../../failures");
+        public void HandleFailureFile ()
+        {
+            var failuresDir = Path.GetFullPath ("../../failures");
 
-			var fixtureName = TestContext.CurrentContext.Test.FullName;
+            var fixtureName = TestContext.CurrentContext.Test.FullName;
 
-			var failureFile = Path.Combine(failuresDir, fixtureName + ".txt");
+            var failureFile = Path.Combine (failuresDir, fixtureName + ".txt");
 
-			if (TestContext.CurrentContext.Result.State == TestState.Error
-			  || TestContext.CurrentContext.Result.State == TestState.Failure)
-			{
-				Console.WriteLine("Test failed.");
+            if (TestContext.CurrentContext.Result.State == TestState.Error
+            || TestContext.CurrentContext.Result.State == TestState.Failure) {
+                Console.WriteLine ("Test failed.");
 
-				Console.WriteLine(failuresDir);
-				Console.WriteLine(fixtureName);
-				Console.WriteLine(failureFile);
+                Console.WriteLine (failuresDir);
+                Console.WriteLine (fixtureName);
+                Console.WriteLine (failureFile);
 
-				if (!Directory.Exists(failuresDir))
-					Directory.CreateDirectory(failuresDir);
+                if (!Directory.Exists (failuresDir))
+                    Directory.CreateDirectory (failuresDir);
 
-				File.WriteAllText(failureFile, fixtureName);
-			}
-			else
-			{
-				Console.WriteLine("Test passed.");
-				if (File.Exists(failureFile))
-					File.Delete(failureFile);
-			}
-		}
+                File.WriteAllText (failureFile, fixtureName);
+            } else {
+                Console.WriteLine ("Test passed.");
+                if (File.Exists (failureFile))
+                    File.Delete (failureFile);
+            }
+        }
 
-		public string GetDevicePort()
-		{
-			var devicePort = Environment.GetEnvironmentVariable("MONITOR_PORT");
+        public string GetDevicePort ()
+        {
+            var devicePort = Environment.GetEnvironmentVariable ("LIGHT_PR_MONITOR_PORT");
 
-			if (String.IsNullOrEmpty(devicePort))
-				devicePort = "/dev/ttyUSB0";
+            if (String.IsNullOrEmpty (devicePort))
+                devicePort = "/dev/ttyUSB0";
 
-			Console.WriteLine("Device port: " + devicePort);
+            Console.WriteLine ("Device port: " + devicePort);
 
-			return devicePort;
-		}
+            return devicePort;
+        }
 
 
-		public string GetSimulatorPort()
-		{
-			var simulatorPort = Environment.GetEnvironmentVariable("MONITOR_SIMULATOR_PORT");
+        public string GetSimulatorPort ()
+        {
+            var simulatorPort = Environment.GetEnvironmentVariable ("LIGHT_PR_MONITOR_SIMULATOR_PORT");
 
-			if (String.IsNullOrEmpty(simulatorPort))
-				simulatorPort = "/dev/ttyUSB1";
+            if (String.IsNullOrEmpty (simulatorPort))
+                simulatorPort = "/dev/ttyUSB1";
 
-			Console.WriteLine("Simulator port: " + simulatorPort);
+            Console.WriteLine ("Simulator port: " + simulatorPort);
 
-			return simulatorPort;
-		}
+            return simulatorPort;
+        }
 
-		public int GetDeviceSerialBaudRate()
-		{
-			return 9600;
-		}
+        public int GetDeviceSerialBaudRate ()
+        {
+            return 9600;
+        }
 
-		public int GetSimulatorSerialBaudRate()
-		{
-			return 9600;
-		}
-	}
+        public int GetSimulatorSerialBaudRate ()
+        {
+            return 9600;
+        }
+    }
 }
